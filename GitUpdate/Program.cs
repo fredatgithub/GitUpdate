dde1bd95d3c71d4ad6f24dcd79257b8aec699ef5
Fred <domaine75@gmail.com>
2019-02-22 20:12:48 +0100

    Add pause method

--- GitUpdate/Program.cs
+++ GitUpdate/Program.cs
@@ -2,6 +2,8 @@
 
 namespace GitUpdate
 {
+  using System.IO;
+
   internal class Program
   {
     private static void Main(string[] arguments)
@@ -27,6 +29,16 @@ namespace GitUpdate
 
       display("press any key to exit:");
       Console.ReadKey();
+    }
+
+    private void AddPauseToFile(string fileName)
+    {
+      const bool append = true;
+      var sw = new StreamWriter(fileName, append);
+      sw.WriteLine(string.Empty);
+      sw.WriteLine("REM Press a key to exit");
+      sw.WriteLine("pause");
+      sw.Close();
     }
   }
 }
