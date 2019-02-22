f541c36cb40f18c7f0e6a3a3334f4e6cae57fa20
Fred <domaine75@gmail.com>
2019-02-22 19:35:18 +0100

    Initial commit

---
+++ GitUpdate/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace GitUpdate
+{
+  internal class Program
+  {
+    private static void Main(string[] arguments)
+    {
+      Action<string> display = Console.WriteLine;
+
+      // TODO add cmd start process to
+      // git add -m "update files"
+      string message = "Update files";
+      if (arguments.Length == 0)
+      {
+        message = arguments[0];
+      }
+
+      // then git commit
+      // then git push origin master
+      display("press any key to exit:");
+      Console.ReadKey();
+    }
+  }
+}
